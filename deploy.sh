c26e5710b6174d0e886914721f7e21dd733ee08d
pbarros <pedro.barros@nokia.com>
2022-05-06 15:52:06 +0100

    Add path to nginx controller

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,8 @@ IMAGE="demo:1.0.0"
 
 sed "s/__IMAGE__/$IMAGE/g;s/__PORT__/$PORT/g" demo.yaml | kubectl apply -f -
 kubectl patch cm tcp-services -ningress-nginx --patch="{\"data\":{\"${PORT}\":\"demo/demo-service:${PORT}\"}}"
+kubectl patch deployment ingress-nginx-controller --patch "$(cat ingress-nginx-patch.yaml | sed 's/__PORT__/${PORT}/g')" -ningress-nginx
 
-grep "demo.net" /etc/hosts || sudo -- sh -c "echo -e '$(minikube ip | awk 'NR==1')\tdemo.net' >> /etc/hosts"; 
+#grep "demo.net" /etc/hosts || sudo -- sh -c "echo -e '$(minikube ip | awk 'NR==1')\tdemo.net' >> /etc/hosts";
+echo "run: telnet $(minikube ip) ${PORT}"
+#telnet $(minikube ip) ${PORT}
